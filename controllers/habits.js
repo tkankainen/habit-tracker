bffdc9f7979649f599a8b0f5460e4348b22d6696
Taina <kankainen.taina@gmail.com>
2023-04-15 21:46:05 +0300

    habit: get /id, put, delete

--- controllers/habits.js
+++ controllers/habits.js
@@ -6,6 +6,18 @@ habitRouter.get('/', (request, response) => {
         response.json(habits)
     })
 })
+
+habitRouter.get('/:id', (request, response, next) => {
+    Habit.findById(request.params.id)
+      .then(habit => {
+        if (habit) {
+          response.json(habit)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error => next(error))
+})
   
 habitRouter.post('/', (request, response) => {
     const body = request.body
@@ -24,6 +36,30 @@ habitRouter.post('/', (request, response) => {
         response.json(savedHabit)
     })
     .catch(error => next(error))
+})
+
+habitRouter.delete('/:id', (request, response, next) => {
+    Habit.findByIdAndRemove(request.params.id)
+      .then(() => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+})
+  
+habitRouter.put('/:id', (request, response, next) => {
+    const body = request.body
+
+    const habit = {
+        name: body.name,
+        targetFrequency: body.targetFrequency,
+        currentStreak: body.currentStreak
+    }
+
+    Habit.findByIdAndUpdate(request.params.id, habit, { new: true })
+        .then(updatedHabit => {
+        response.json(updatedHabit)
+        })
+        .catch(error => next(error))
 })
 
 module.exports = habitRouter
