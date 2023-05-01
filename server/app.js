bc7b346102e9157d6f1b28c35a70c5d784770f96
Taina <kankainen.taina@gmail.com>
2023-05-01 19:03:34 +0300

    kansiorakenne server-client

--- server/app.js
+++ server/app.js
@@ -20,14 +20,10 @@ mongoose.connect(config.MONGODB_URI)
     })
 
 app.use(cors())
-//app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/habits', habitRouter)
 app.use('/api/logs', logRouter)
-
-//app.use(middleware.unknownEndpoint)
-//app.use(middleware.errorHandler)
 
 module.exports = app
