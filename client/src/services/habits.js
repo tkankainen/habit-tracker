9ab24323bdb92b6507d7ba755ac754c5a471f1fd
Taina <kankainen.taina@gmail.com>
2023-05-03 18:34:23 +0300

    päivitetty habits.js ja logs.js

--- client/src/services/habits.js
+++ client/src/services/habits.js
@@ -2,25 +2,24 @@ import axios from 'axios'
 
 const url = 'http://localhost:3001/api/habits'
 
-const getAll = () => {
-    const request = axios.get(url)
-    return request.then(response => response.data)
+const habitService = {
+    getAll: () => {
+        const request = axios.get(url)
+        return request.then(response => response.data)
+    },
+    create: async newObject => {
+        const response = await axios.post(url, newObject)
+        return response.data
+    },
+    update: (id, newObject) => {
+        const request = axios.put(`${url}/${id}`, newObject)
+        return request.then(response => response.data)
+    },
+    remove: (id) => {
+        const request = axios.delete(`${url}/${id}`)
+        return request.then(response => response.data)
+    }
 }
 
-const create = async newObject => {
-    const response = await axios.post(url, newObject)
-    return response.data
-}
-
-const update = (id, newObject) => {
-    const request = axios.put(`${url}/${id}`, newObject)
-    return request.then(response => response.data)
-}
-
-const remove = (id) => {
-    const request = axios.delete(`${url}/${id}`)
-    return request.then(response => response.data)
-}
-
-export default { getAll, create, update, remove }
+export default habitService
 
--- client/src/services/logs.js
+++ client/src/services/logs.js
@@ -2,15 +2,16 @@ import axios from 'axios'
 
 const url = 'http://localhost:3001/api/logs'
 
-const getAll = () => {
-    const request = axios.get(url)
-    return request.then(response => response.data)
+const logService = {
+    getAll: () => {
+      const request = axios.get(url)
+      return request.then(response => response.data)
+    },
+    create: async newObject => {
+      const response = await axios.post(url, newObject)
+      return response.data
+    }
 }
-
-const create = async newObject => {
-    const response = await axios.post(url, newObject)
-    return response.data
-}
-
-export default { getAll, create }
+  
+export default logService
 
